0ef36d79057946777d77fdb43007adac85d7b06e
Paul Patarinski <paulpatarinski@gmail.com>
2015-10-18 21:53:08 -0500

    Upgrading to AppCompat per https://gist.github.com/jassmith/a3b2a543f99126782936

--- XF_AppCompat/Droid/MainActivity.cs
+++ XF_AppCompat/Droid/MainActivity.cs
@@ -7,14 +7,19 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
 
 namespace XF_AppCompat.Droid
 {
 	[Activity (Label = "XF_AppCompat.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	public class MainActivity : FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
+			// Must be called before base.OnCreate (bundle);
+			ToolbarResource = Resource.Layout.Toolbar;
+			TabLayoutResource = Resource.Layout.TabLayout;
+
 			base.OnCreate (bundle);
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
