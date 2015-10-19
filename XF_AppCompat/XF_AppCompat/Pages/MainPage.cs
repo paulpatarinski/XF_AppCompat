ad489dda4f0fd030fd9483b0ed9c447dde0d28e3
Paul Patarinski <paulpatarinski@gmail.com>
2015-10-18 20:46:32 -0500

    Adding a Sample Master Detail Page

---
+++ XF_AppCompat/XF_AppCompat/Pages/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_AppCompat.Pages
+{
+	public class MainPage : Xamarin.Forms.MasterDetailPage
+	{
+		public MainPage ()
+		{
+
+			var menuPageContent = new StackLayout {
+				VerticalOptions = LayoutOptions.Center,
+			};
+
+			menuPageContent.Children.Add (new Label {
+				Text = "Menu Drawer",
+				TextColor = Color.White,
+				HorizontalOptions = LayoutOptions.Center
+			});
+
+			var menuPage = new ContentPage { Content = menuPageContent, Title = "Menu Drawer"
+			};
+
+			Master = menuPage;
+
+
+			var detailPageContent = new StackLayout { VerticalOptions = LayoutOptions.Center };
+
+			detailPageContent.Children.Add (new Label {
+				Text = "Landing Page",
+				TextColor = Color.Aqua,
+				HorizontalOptions = LayoutOptions.Center
+			});
+
+			var detailPage = new ContentPage { Content = detailPageContent, Title = "Home" };
+
+
+			Detail = new NavigationPage (detailPage);
+
+		}
+	}
+}
+
